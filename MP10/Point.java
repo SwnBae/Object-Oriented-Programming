c0417547472c78516d979174d6bd0d1bf8f5b116
SwnBae <128581113+SwnBae@users.noreply.github.com>
2024-10-08 10:48:01 +0900

    테트리스 블록 만들고 출력하기

---
+++ MP10/Point.java
@@ -0,0 +1,22 @@
+class Point {
+    private int x;
+    private int y;
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    @Override
+    public String toString() {
+        return "(" + getX() + "," + getY() + ")";
+    }
+}
