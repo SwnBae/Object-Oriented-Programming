a169b018acc4edf2b5237eb16cb9fe3a627a2b72
SwnBae <128581113+SwnBae@users.noreply.github.com>
2024-10-08 11:04:09 +0900

    MP10-테트리스 블록 만들고 출력하기

---
+++ MP10/Shape.java
@@ -0,0 +1,14 @@
+class Shape {
+    public double getArea() {
+        return 0.0;
+    }
+
+    public double getPerimeter() {
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "shape";
+    }
+}
